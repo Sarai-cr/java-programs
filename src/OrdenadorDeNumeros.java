bb5d1d5071b2da0c3c341953b319cf643a647548
Jose Cruz <jf.cruz001@ebc.edu.mx>
2020-03-31 00:00:13 -0600

    App que ordena números de menor a mayor

---
+++ src/OrdenadorDeNumeros.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class OrdenadorDeNumeros {
+
+  public static void main (String args []){
+    Scanner leer = new Scanner (System.in);
+    System.out.println ("Ingresa un número entero");
+    int n1 = leer.nextInt();
+    System.out.println ("Ingresa otro número entero");
+    int n2 = leer.nextInt();
+    System.out.println ("Ingresa otro número entero");
+    int n3 = leer.nextInt();
+    ordenar(n1, n2, n3);
+  }
+
+  public static void ordenar(int n1, int n2, int n3) {
+    
+  }
+
+}
+
