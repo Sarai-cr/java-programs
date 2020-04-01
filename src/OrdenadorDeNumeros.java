59e34d4a98e0aa551622bb900ac06fbeabd1dc0c
Jose Cruz <jf.cruz001@ebc.edu.mx>
2020-03-31 22:38:20 -0600

    Ordenador de números

--- src/OrdenadorDeNumeros.java
+++ src/OrdenadorDeNumeros.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class OrdenadorDeNumeros {
@@ -10,11 +11,39 @@ public class OrdenadorDeNumeros {
     int n2 = leer.nextInt();
     System.out.println ("Ingresa otro número entero");
     int n3 = leer.nextInt();
-    ordenar(n1, n2, n3);
+    int[] numerosOrdenados = ordenar(n1, n2, n3);
+    System.out.println("El orden de menor a mayor es: " +
+      numerosOrdenados[0] + " " +
+      numerosOrdenados[1] + " " +
+      numerosOrdenados[2]);
+    int [] mayorAMenor= voltearArreglo(numerosOrdenados);
+    System.out.println("El orden de mayor a menor es: " +
+      mayorAMenor[0] + " " +
+      mayorAMenor[1] + " " +
+      mayorAMenor[2]);
   }
 
-  public static void ordenar(int n1, int n2, int n3) {
-    
+  public static int[] ordenar(int n1, int n2, int n3) {
+    if (n1>n2 && n1>n3 && n2>n3){
+      return new int[] {n3,n2,n1};
+    }else if(n1>n2 && n1>n3 && n3>n2){
+      return new int[] {n2,n3,n1};
+    }else if (n2>n1 && n2>n3 && n1>n3){
+      return new int[] {n3,n1,n2};
+    }else if(n2>n1 && n2>n3 && n3>n1){
+      return new int[] {n1,n3,n2};
+    }else if(n3>n1 && n3>n2 && n2>n1){
+      return new int[] {n1,n2,n3};
+    }else{
+      return new int[] {n2,n1,n3};
+    }
+  }
+
+  public static int[] voltearArreglo(int[] arreglo) {
+    int temporal= arreglo[0];
+    arreglo[0] = arreglo[2];
+    arreglo[2]= temporal;
+    return arreglo;
   }
 
 }
