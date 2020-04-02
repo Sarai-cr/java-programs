8c0e4e469b0f3549cb7a7faa9178eee56c69f319
Sarai-cr <62923726+Sarai-cr@users.noreply.github.com>
2020-04-02 11:38:17 -0600

    Add files via upload

---
+++ BucleFor.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class BucleFor {
+    public static void main (String []args){
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Numeración del 1 al 10");
+        int i=0;
+        for ( i=1 ; i<=10 ; i++ ){
+            System.out.println(i);
+        }
+    }
+}
