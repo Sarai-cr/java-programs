ed85b6c12c40213de5a39d9753038e58a88f0477
Sarai-cr <62923726+Sarai-cr@users.noreply.github.com>
2020-04-02 10:44:19 -0600

    Add files via upload

---
+++ BucleWhile.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class BucleWhile {
+    public static void main (String []args){
+        Scanner entrada= new Scanner(System.in);
+        System.out.println("Numeración del 1 al 10");
+        int i=1;
+        while (i<=10){
+            System.out.println(i);
+            i++;
+        }
+
+    }
+}
