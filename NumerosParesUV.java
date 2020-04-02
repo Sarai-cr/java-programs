1e901c797c0032efce793f900a1c5404cc42a247
Sarai-cr <62923726+Sarai-cr@users.noreply.github.com>
2020-04-02 12:34:03 -0600

    Add files via upload

---
+++ NumerosParesUV.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class NumerosParesUV {
+    public static void main (String []args){
+        Scanner entrada = new Scanner (System.in);
+        int n=1;
+        while(n <= 25){
+            if(n%2==0){
+                System.out.println(n);
+            }
+            n++;
+        }
+        }
+    }
+
