9aeead1579be84d8506b56ec9eb32371145bb2dc
Sarai-cr <62923726+Sarai-cr@users.noreply.github.com>
2020-04-17 13:12:35 -0500

    Add files via upload

--- ArregloAlumnos.java
+++ ArregloAlumnos.java
@@ -2,32 +2,41 @@ import java.util.Scanner;
 public class ArregloAlumnos {
     public static void main(String[] args) {
         Scanner leer = new Scanner(System.in);
-        String []nombres = new String [2];
-        double []promedios = new double [2];
-        String []notasEnTexto = new String[4];
+        String[] nombres = new String[2];
+        double[] promedios = new double[2];
+        String[] notasEnTexto = new String[2];
 
-        for (int i=0; i<2;i++){
-            System.out.println("Ingresa el nombre del alumno  "+i);
-            nombres[i]=leer.nextLine();
+        for (int i = 0; i < 2; i++) {
+            System.out.println("Ingresa el nombre del alumno  ");
+            nombres[i] = leer.nextLine();
         }
 
-        for (int i=0; i<2;i++){
-            System.out.println("Ingresa el promedio del alumno  "+i);
-            promedios[i]=leer.nextDouble();
+        for (int i = 0; i < 2; i++) {
+            System.out.println("Ingresa el promedio del alumno  ");
+            promedios[i] = leer.nextDouble();
         }
-        
-            // if ((promedios>0)&&(promedios<=4.99)){
-            //notasEnTexto[0]="Suspenso";
-       // }else if ((promedios>5)&&(promedios<=6.99)){
-            //notasEnTexto[1]="Bien";
-       // }else if ((promedios>7)&&(promedios<=8.99)){
-            //notasEnTexto[2]="Notable";
-        //}else if ((promedios>9)&&(promedios<=10)){
-           // notasEnTexto[3]="Sobresaliente";
-        //}
-        System.out.println(notasEnTexto[0]);
-        System.out.println(notasEnTexto[1]);
-        System.out.println(notasEnTexto[2]);
-        System.out.println(notasEnTexto[3]);
+
+        for (int p = 0; p < 2; p++) {
+            if (promedios[p] >= 0 && promedios[p] <= 4.99) {
+                notasEnTexto[p] = "Suspenso";
+            } else if (promedios[p] >= 5 && promedios[p] <= 6.99) {
+                notasEnTexto[p] = "Bien";
+            } else if (promedios[p] >= 7 && promedios[p] <= 8.99) {
+                notasEnTexto[p] = "Notable";
+            } else if (promedios[p] >= 9 && promedios[p] <= 10) {
+                notasEnTexto[p] = "Sobresaliente";
+            }
+        }
+        System.out.println(" Los nombres y notas de los alumnos son las siguientes: ");
+        for (int i = 0; i < 2; i++) {
+            System.out.println("\n" + nombres[i] + ": \t");
+            for (int p = 0; p < 2; p++) {
+                System.out.println(promedios[p] + "\t");
+
+
+            }
+
+        }
+
     }
-    }
+}
